e3022e92ce420e487cd3a103b1dae9da65d3e422
Michael <88445485+mjnyland@users.noreply.github.com>
2023-09-08 15:40:45 -0400

    adjusting router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/daysheetsv2" element={<Home />} />
+        <Route path="/web-ui-v2" element={<Home />} />
         <Route path="/help" element={<Help />} />
         <Route path="/pricing" element={<Prices />} />
       </Routes>
