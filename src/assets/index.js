976149d013e7c2fe9b4cb37d969e9c833140a6b0
Michael <88445485+mjnyland@users.noreply.github.com>
2023-09-11 10:04:05 -0400

    Removed light gradient

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,13 +17,12 @@ import avatar from "./margeavatar.png";
 import dragAndDropFlight from "./dragAndDropFlight.png";
 import flightGrid from "./macbookFlightGrid.png";
 import checkmark from "./checkmark.svg";
-import checkmark_white from "./checkmark_w.svg"
+import checkmark_white from "./checkmark_w.svg";
 import logotype from "./logotype.svg";
 import logo from "./daysheets_mac_icon.png";
 import iPhoneMockup from "./iPhonemockup.png";
 import iPhoneMockup2 from "./iPhonemockup2.png";
 import iPhoneMockup3 from "./iPhonemockup3.png";
-import lightGradient from "./daysheets-lightGradient.png";
 import flightGridMock from "./flightgrid-mock.png";
 import flightIcon from "./flightIcon.svg";
 import travelProfileUI from "./travelprofile-ui.png";
@@ -32,12 +31,11 @@ import profileRequest from "./profile-requests.png";
 import profileExport from "./profile-export.png";
 import profileExportM from "./profile-exports-mobile.png";
 import profileRequestM from "./profile-requests-mobile.png";
-import gradient from "./gradientBG.png"
-import chatBubble1 from "./chatBubble_1.png"
-import chatBubble2 from "./chatBubble_2.png"
-import chatBubble3 from "./chatBubble_3.png"
-import chatBubbles_M from "./chatBubbles_M.png"
-
+import gradient from "./gradientBG.png";
+import chatBubble1 from "./chatBubble_1.png";
+import chatBubble2 from "./chatBubble_2.png";
+import chatBubble3 from "./chatBubble_3.png";
+import chatBubbles_M from "./chatBubbles_M.png";
 
 export {
   flightGrid,
@@ -65,7 +63,6 @@ export {
   iPhoneMockup,
   iPhoneMockup2,
   iPhoneMockup3,
-  lightGradient,
   flightGridMock,
   flightIcon,
   travelProfileUI,
@@ -78,5 +75,5 @@ export {
   chatBubble1,
   chatBubble2,
   chatBubble3,
-  chatBubbles_M
+  chatBubbles_M,
 };
